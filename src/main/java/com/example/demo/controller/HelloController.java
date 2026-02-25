9883097eaf810a8428fb82e7b0e71ad065ea4428
Hemanth <hemanth@gmail.com>
2026-02-25 10:23:23 +0530

    Initial commit including Dockerfile

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot is running successfully 🚀";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
